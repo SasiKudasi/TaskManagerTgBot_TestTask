a66e3ddeaa55ce1bba14bfc07d419d1ec0b800fa
sasaikudasai <sasha.mittal@yandex.ru>
2024-06-27 17:09:31 +0000

    Initial commit

---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTgBot
+{
+
+    public class User
+    {
+        public long Id { get; set; }
+        public string Message { get; set; }
+
+
+        public User(long id, string message)
+        {
+
+            Id = id;
+            Message = message;
+        }
+
+    }
+}
